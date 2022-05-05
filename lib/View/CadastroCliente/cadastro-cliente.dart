d300c0e039156915b1d04acccbfe3441e70020e5
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-05-05 18:02:19 -0300

    Dando início na criação da tela de cadastro de usuário

--- lib/View/CadastroCliente/cadastro-cliente.dart
+++ lib/View/CadastroCliente/cadastro-cliente.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:pointsf/components/button-primary.dart';
+import 'package:pointsf/components/text-input-personalizado.dart';
 
 class CadastroCliente extends StatelessWidget {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final TextEditingController _controladorNome = TextEditingController();
+  final TextEditingController _controladorEmail = TextEditingController();
+  final TextEditingController _controladorTelefone = TextEditingController();
+  final TextEditingController _controladorCpf = TextEditingController();
+  final TextEditingController _controladorSenha = TextEditingController();
+  final TextEditingController _controladorConfirmarSenha =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 254, 220, 86),
+        backgroundColor: const Color.fromARGB(255, 254, 220, 86),
         actions: [
           Image.asset(
             'images/logo-escrita.png',
@@ -13,11 +25,48 @@ class CadastroCliente extends StatelessWidget {
             height: 30,
           ),
         ],
-        leading: Icon(
+        leading: const Icon(
           Icons.arrow_back,
         ),
       ),
-      body: Text("Olá mundo"),
+      body: Form(
+        key: formKey,
+        child: ListView(
+          children: <Widget>[
+            TextInputPersonalizado(
+              controlador: _controladorNome,
+              descricaoCampo: 'Nome',
+              placeholder: 'Gustavo de Freitas',
+            ),
+            TextInputPersonalizado(
+              controlador: _controladorEmail,
+              descricaoCampo: 'E-mail',
+              placeholder: 'email@email.com',
+            ),
+            TextInputPersonalizado(
+              controlador: _controladorTelefone,
+              descricaoCampo: 'Telefone',
+              placeholder: '(17) 99999-9999',
+            ),
+            TextInputPersonalizado(
+              controlador: _controladorCpf,
+              descricaoCampo: 'CPF',
+              placeholder: '999.999.999-99',
+            ),
+            TextInputPersonalizado(
+              controlador: _controladorSenha,
+              descricaoCampo: 'Senha',
+              placeholder: '*********',
+            ),
+            TextInputPersonalizado(
+              controlador: _controladorConfirmarSenha,
+              descricaoCampo: 'Confirmar senha',
+              placeholder: '*********',
+            ),
+            ButtonPrimary('Cadastrar'),
+          ],
+        ),
+      ),
     );
   }
 }

e3c5afbb2f7fb90e44a1d03aa3f6525baf5176b8
Heitor Piva Carreira <heitorpiva015@gmail.com>
2022-05-05 17:20:00 -0300

    Foi criado o model do cadastro do produto e foi criado os seus atributos

---
+++ lib/model/cadastro-produto-model.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+
+class cadastroProdutoModel{
+  String? nome;
+  String? tipo;
+  bool? status;
+  String? categoria;
+  String? medida;
+
+}
+
+
+
+
+

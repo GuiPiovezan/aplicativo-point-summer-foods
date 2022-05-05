d01e1134bfa5c87304f8223dc2a841a3ff785e56
Heitor Piva Carreira <heitorpiva015@gmail.com>
2022-05-05 17:48:08 -0300

    Instalacao do Cloud Firestore e do Firebase Auth

--- lib/model/cadastro-produto-model.dart
+++ lib/model/cadastro-produto-model.dart
@@ -2,14 +2,28 @@
 import 'package:flutter/material.dart';
 
 
-class cadastroProdutoModel{
+class CadastroProdutoModel{
   String? nome;
-  String? tipo;
+  String? tipoProdutoOuAdicional;
   bool? status;
   String? categoria;
   String? medida;
 
+  CadastroProdutoModel(this.nome, this.tipoProdutoOuAdicional, this.status, this.categoria, this.medida);
+
+  CadastroProdutoModel.fromMap(Map<String, dynamic> map){
+    nome = map['nome'];
+    tipoProdutoOuAdicional = map['tipo'];
+    status = map['status'];
+    categoria = map['categoria'];
+    medida = map['medida'];
+  }
 }
+
+
+
+
+
 
 
 

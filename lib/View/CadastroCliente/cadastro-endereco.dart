8a7635f23da54a45bca2c5850c3471ac49ab1f55
Vitor-Kemuel <kemuvitor@gmail.com>
2022-05-05 17:57:31 -0300

    Adicionando input de CEP

--- lib/View/CadastroCliente/cadastro-endereco.dart
+++ lib/View/CadastroCliente/cadastro-endereco.dart
@@ -4,15 +4,31 @@ class CadastroEndereco extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 254, 220, 86),
+      appBar: AppBar(
+        actions: [
+          Image.asset(
+            'images/logo-escrita.png',
+            width: 100,
+            height: 50,
+          ),
+        ],
         backgroundColor: Color.fromARGB(255, 254, 220, 86),
-        AppBar: AppBar(
-          actions: [
-            Image.asset(
-              'images/logo-escrita',
-              width: 100,
-              height: 50,
-            )
-          ],
-        ));
+        title: Center(
+          child: Text("Endereço"),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "CEP",
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }

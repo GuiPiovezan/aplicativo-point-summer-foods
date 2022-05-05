77f39e1dca35924747cbc039972cf29da4a7130c
AlexSan <alex.gsan11@gmail.com>
2022-05-05 14:32:26 -0300

    Resolvendo erro

--- lib/View/CadastroProduto/cadastro-produto.dart
+++ lib/View/CadastroProduto/cadastro-produto.dart
@@ -1,24 +1,18 @@
 import 'dart:html';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
-
-class CadastroProduto extends StatelessWidget {
+class CadastroProduto extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          const Color.fromARGB(255, 254, 220, 86), //Cor na tela Toda
+      backgroundColor: const Color.fromARGB(255,254,220,86), //Cor na tela Toda 
       appBar: AppBar(
-        backgroundColor:
-            const Color.fromARGB(255, 254, 220, 86), //Cor no App Bar
+        backgroundColor:const Color.fromARGB(255,254,220,86) , //Cor no App Bar
         title: Center(
-          child: Text(
-            "Cadastro de Produtos",
-            textAlign: TextAlign.end,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.purple,
+          child: Text("Cadastro de Produtos",
+          textAlign: TextAlign.end,
+            style: TextStyle(fontSize: 20,
+            color: Colors.purple,
             ),
           ),
         ),
@@ -28,10 +22,9 @@ class CadastroProduto extends StatelessWidget {
           color: Colors.purple,
         ),
         actions: [
-          Image.asset(
-            'images/logo-escrita.png',
-            width: 100,
-            height: 30,
+           Image.asset('images/logo-escrita.png',
+           width: 100,
+           height: 30,
           ),
         ],
       ),
@@ -39,12 +32,16 @@ class CadastroProduto extends StatelessWidget {
         child: Container(
           child: Column(
             children: [
-<<<<<<< HEAD
+              Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0)),
               Padding(padding: EdgeInsets.fromLTRB(50, 15, 50, 15)),
               Image.asset("images/logo.png",
               width: 180,
               height: 180,
               ),
+              TextField(decoration: InputDecoration(
+                //border: OutlinedBorder(side:),
+                hintText: "nome"),),
+
               TextFormField(
                 decoration: InputDecoration(
                   hintText: "Nome",
@@ -84,24 +81,10 @@ class CadastroProduto extends StatelessWidget {
                   ),
                 ),
               ),
-=======
-              Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0)),
-              Image.asset(
-                "images/logo.png",
-                width: 180,
-                height: 180,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    //border: OutlinedBorder(side:),
-                    hintText: "nome"),
-              ),
->>>>>>> 9add534be604c2d762620fcb693ff69e868f2f2c
             ],
           ),
         ),
       ),
-      //bottomNavigationBar: ,
     );
   }
 }

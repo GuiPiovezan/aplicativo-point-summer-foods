c6d3fe517f6ab0f4a4957eb32157a2817f93bc36
Vitor-Kemuel <kemuvitor@gmail.com>
2022-05-07 21:43:37 -0300

    Alterando front da tela de cadastro de endereço

--- lib/View/CadastroCliente/cadastro-endereco.dart
+++ lib/View/CadastroCliente/cadastro-endereco.dart
@@ -15,18 +15,51 @@ class CadastroEndereco extends StatelessWidget {
         ],
         backgroundColor: Color.fromARGB(255, 254, 220, 86),
         title: Center(
-          child: Text("Endereço"),
+          child: Text(
+            "Endereço",
+            style: TextStyle(color: Colors.black),
+          ),
         ),
       ),
       body: Center(
         child: Container(
-          child: Column(children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: "CEP",
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 15, 5, 10),
+                width: 150,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "CEP",
+                  ),
+                ),
               ),
-            ),
-          ]),
+              TextButton(
+                onPressed: () => {},
+                child: Text(
+                  "Buscar CEP",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.all(15),
+                  backgroundColor: Color.fromARGB(255, 83, 5, 64),
+                  side: BorderSide(
+                    color: Colors.white,
+                    width: 1,
+                    style: BorderStyle.solid,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(15.0),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

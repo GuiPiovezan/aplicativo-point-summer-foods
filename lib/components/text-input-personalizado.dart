8449eb1edc08ac7238f9cf5bec4032c688518bdd
Vitor-Kemuel <kemuvitor@gmail.com>
2022-05-09 16:17:06 -0300

    Alteração nos Widgets personalizados

--- lib/components/text-input-personalizado.dart
+++ lib/components/text-input-personalizado.dart
@@ -10,16 +10,50 @@ class TextInputPersonalizado extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: TextField(
-        controller: controlador,
-        style: const TextStyle(fontSize: 24.0),
-        decoration: InputDecoration(
-          labelText: descricaoCampo,
-          hintText: placeholder,
+    // return Padding(
+    //   padding: const EdgeInsets.all(10.0),
+    //   child: TextField(
+    //     controller: controlador,
+    //     style: const TextStyle(fontSize: 24.0),
+    //     decoration: InputDecoration(
+    //       labelText: descricaoCampo,
+    //       hintText: placeholder,
+    //     ),
+    //     keyboardType: TextInputType.number,
+    //   ),
+    // );
+    return Center(
+      child: Container(
+        margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+        width: 320,
+        height: 60,
+        child: TextField(
+          controller: controlador,
+          keyboardType: TextInputType.number,
+          style: const TextStyle(fontSize: 24.0),
+          decoration: InputDecoration(
+            labelText: descricaoCampo,
+            hintText: placeholder,
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color.fromARGB(255, 83, 5, 64),
+                width: 2,
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.black,
+                width: 2,
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+            ),
+          ),
         ),
-        keyboardType: TextInputType.number,
       ),
     );
   }

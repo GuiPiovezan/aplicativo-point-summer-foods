e4b6dfe6ac195e273684b3f78b5595c5c5b49555
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-05-09 21:41:35 -0300

    Alterando definição da classe de usuário

--- lib/View/CadastroCliente/cliente.dart
+++ lib/View/CadastroCliente/cliente.dart
@@ -1,13 +1,10 @@
-import 'dart:html';
-
 class Cliente {
-  String? nome;
-  String? email;
-  String? telefone;
-  String? cpf;
-  String? senha;
-
-  void salvar(Cliente cliente) {
-    print('Nome: $cliente.nome');
+  void salvar(
+      {required nome,
+      required email,
+      required telefone,
+      required cpf,
+      required senha}) {
+    print(nome);
   }
 }

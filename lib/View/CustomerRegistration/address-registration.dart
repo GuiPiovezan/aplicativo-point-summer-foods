3b384e62a480da4d0604c23a6b43daa9ab0b1565
Vitor-Kemuel <kemuvitor@gmail.com>
2022-05-12 01:23:00 -0300

    Adicionando função para desativar e ativar os campos conforme o CEP digitado

--- lib/View/CustomerRegistration/address-registration.dart
+++ lib/View/CustomerRegistration/address-registration.dart
@@ -18,6 +18,11 @@ class _AddressRegistration extends State<AddressRegistration> {
   TextEditingController _controladorComplemento = TextEditingController();
   TextEditingController _controladorBairro = TextEditingController();
   TextEditingController _controladorCidade = TextEditingController();
+  bool enableLogradouro = true;
+  bool enableNumero = true;
+  bool enableComplemento = true;
+  bool enableBairro = true;
+  bool enableCidade = true;
 
   Future<void> searchCEP() async {
     String cep = txtCEP.text;
@@ -29,15 +34,57 @@ class _AddressRegistration extends State<AddressRegistration> {
     print('Resposta:' + response.body);
 
     print('Resposta Servidor:' + response.statusCode.toString());
-    Map<String, dynamic> dados = json.decode(response.body);
 
-    setState(() {
-      _controladorLogradouro = TextEditingController(text: dados["logradouro"]);
-      _controladorComplemento =
-          TextEditingController(text: dados["complemento"]);
-      _controladorBairro = TextEditingController(text: dados["bairro"]);
-      _controladorCidade = TextEditingController(text: dados["localidade"]);
-    });
+    if (response.statusCode.toString() == "200") {
+      Map<String, dynamic> dados = json.decode(response.body);
+
+      setState(() {
+        if (dados["logradouro"] != "") {
+          _controladorLogradouro =
+              TextEditingController(text: dados["logradouro"]);
+          enableLogradouro = false;
+        } else {
+          _controladorLogradouro = TextEditingController(text: "");
+          enableLogradouro = true;
+        }
+
+        if (dados["complemento"] != "") {
+          _controladorComplemento =
+              TextEditingController(text: dados["complemento"]);
+          enableComplemento = false;
+        } else {
+          _controladorComplemento = TextEditingController(text: "");
+          enableComplemento = true;
+        }
+
+        if (dados["bairro"] != "") {
+          _controladorBairro = TextEditingController(text: dados["bairro"]);
+          enableBairro = false;
+        } else {
+          _controladorBairro = TextEditingController(text: "");
+          enableBairro = true;
+        }
+
+        if (dados["localidade"] != "") {
+          _controladorCidade = TextEditingController(text: dados["localidade"]);
+          enableCidade = false;
+        } else {
+          _controladorCidade = TextEditingController(text: "");
+          enableCidade = true;
+        }
+      });
+    } else {
+      setState(() {
+        _controladorLogradouro = TextEditingController(text: "");
+        enableLogradouro = true;
+        _controladorComplemento = TextEditingController(text: "");
+        enableComplemento = true;
+        _controladorBairro = TextEditingController(text: "");
+        enableBairro = true;
+        _controladorCidade = TextEditingController(text: "");
+        enableCidade = true;
+      });
+    }
   }
 
   @override
@@ -78,27 +125,32 @@ class _AddressRegistration extends State<AddressRegistration> {
               controlador: _controladorLogradouro,
               descricaoCampo: 'Logradouro',
               placeholder: 'Rua José Pereira',
+              enable: enableLogradouro,
             ),
             CustomTextField(
               controlador: _controladorNumero,
               descricaoCampo: 'Numero',
               placeholder: '547',
               inputType: TextInputType.number,
+              enable: enableNumero,
             ),
             CustomTextField(
               controlador: _controladorComplemento,
               descricaoCampo: 'Complemento',
               placeholder: 'Apartamento 13',
+              enable: enableComplemento,
             ),
             CustomTextField(
               controlador: _controladorBairro,
               descricaoCampo: 'Bairro',
               placeholder: 'Jardim das Flores',
+              enable: enableBairro,
             ),
             CustomTextField(
               controlador: _controladorCidade,
               descricaoCampo: 'Cidade',
               placeholder: 'São José do Rio Preto',
+              enable: enableCidade,
             ),
             CustomTextButton(
               textoBotao: "Cadastrar",

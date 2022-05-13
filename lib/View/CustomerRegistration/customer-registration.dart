7915b76291edcb5ea7d06bbcb2dd2c3f6a59493a
Vitor-Kemuel <kemuvitor@gmail.com>
2022-05-13 20:49:32 -0300

    Corrigindo widget da tela de cadastro de usuario

--- lib/View/CustomerRegistration/customer-registration.dart
+++ lib/View/CustomerRegistration/customer-registration.dart
@@ -47,74 +47,72 @@ class CustomerRegistration extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 254, 220, 86),
-      appBar: CustomAppBar(
+      appBar: const CustomAppBar(
         title: "Customer Register",
       ),
       body: Form(
         key: formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              CustomTextField(
-                onSaved: (value) => nome = value!,
-                descricaoCampo: 'Nome',
-                validator: (value) => UserValidator.validarNome(value!),
-              ),
-              CustomTextField(
-                onSaved: (value) => email = value!,
-                descricaoCampo: 'E-mail',
-                validator: (value) => UserValidator.validarEmail(value!),
-              ),
-              CustomTextField(
-                onSaved: (value) => telefone = value!,
-                descricaoCampo: 'Telefone',
-                inputType: TextInputType.number,
-                validator: (value) => UserValidator.validarTelefone(value!),
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  TelefoneInputFormatter(),
-                ],
-              ),
-              CustomTextField(
-                onSaved: (value) => cpf = value!,
-                descricaoCampo: 'CPF',
-                inputType: TextInputType.number,
-                validator: (value) => UserValidator.validarCPF(value!),
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  CpfInputFormatter()
-                ],
-              ),
-              CustomTextField(
-                onSaved: (value) => senha = value!,
-                descricaoCampo: 'Senha',
-                controlador: senhaController,
-                validator: (value) => UserValidator.validarSenha(value!),
-              ),
-              CustomTextField(
-                onSaved: (value) => confirmarSenha = value!,
-                descricaoCampo: 'Confirmar senha',
-                validator: (value) => UserValidator.validarConfirmarSenha(
-                    value!, senhaController.text),
-              ),
-              CustomTextButton(
-                textoBotao: 'Cadastrar',
-                onPressed: () {
-                  save(context);
-                },
-              ),
-              CustomTextButton(
-                textoBotao: 'Rota dev para endereço',
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => AddressRegistration(),
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
+        child: ListView(
+          children: <Widget>[
+            CustomTextField(
+              onSaved: (value) => nome = value!,
+              descricaoCampo: 'Nome',
+              validator: (value) => UserValidator.validarNome(value!),
+            ),
+            CustomTextField(
+              onSaved: (value) => email = value!,
+              descricaoCampo: 'E-mail',
+              validator: (value) => UserValidator.validarEmail(value!),
+            ),
+            CustomTextField(
+              onSaved: (value) => telefone = value!,
+              descricaoCampo: 'Telefone',
+              inputType: TextInputType.number,
+              validator: (value) => UserValidator.validarTelefone(value!),
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                TelefoneInputFormatter(),
+              ],
+            ),
+            CustomTextField(
+              onSaved: (value) => cpf = value!,
+              descricaoCampo: 'CPF',
+              inputType: TextInputType.number,
+              validator: (value) => UserValidator.validarCPF(value!),
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                CpfInputFormatter()
+              ],
+            ),
+            CustomTextField(
+              onSaved: (value) => senha = value!,
+              descricaoCampo: 'Senha',
+              controlador: senhaController,
+              validator: (value) => UserValidator.validarSenha(value!),
+            ),
+            CustomTextField(
+              onSaved: (value) => confirmarSenha = value!,
+              descricaoCampo: 'Confirmar senha',
+              validator: (value) => UserValidator.validarConfirmarSenha(
+                  value!, senhaController.text),
+            ),
+            CustomTextButton(
+              textoBotao: 'Cadastrar',
+              onPressed: () {
+                save(context);
+              },
+            ),
+            CustomTextButton(
+              textoBotao: 'Rota dev para endereço',
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => AddressRegistration(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );

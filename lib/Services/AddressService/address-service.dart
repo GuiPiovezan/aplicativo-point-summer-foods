7552ccac776bb881519fa21157c45613cf7d807f
Vitor-Kemuel <kemuvitor@gmail.com>
2022-05-17 00:55:36 -0300

    Adicionando arquivo para serviços relacionados aos endereços

---
+++ lib/Services/AddressService/address-service.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:pointsf/Services/AuthService/auth-service.dart';
+import 'package:pointsf/models/address-model.dart';
+
+import 'package:pointsf/View/export-all-view.dart';
+import 'package:uuid/uuid.dart';
+
+class AddressException implements Exception {
+  String message;
+  AddressException(this.message);
+}
+
+class AddressService extends ChangeNotifier {
+  final firestore = FirebaseFirestore.instance;
+
+  registration(AddressModel model, BuildContext context) {
+    var uuid = const Uuid();
+    var uid = uuid.v1();
+    try {
+      firestore
+          .collection('usuarios')
+          .doc(AuthService().getUid())
+          .collection("enderecos")
+          .doc(uid)
+          .set({
+        "cep": model.cep,
+        "logradouro": model.logradouro,
+        "numero": model.numero,
+        "complemento": model.complemento,
+        "bairro": model.bairro,
+        "cidade": model.cidade,
+        "uid": uid,
+      });
+    } on FirebaseException catch (e) {
+      throw Exception(e.code);
+    }
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ListAddress(),
+      ),
+    );
+  }
+}

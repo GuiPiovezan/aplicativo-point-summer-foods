e57c87286ebe2eae4447730bca77a3df6e6d6d5f
Vitor-Kemuel <kemuvitor@gmail.com>
2022-05-19 01:19:26 -0300

    Implementando novo widget

--- lib/View/Home/admin-home.dart
+++ lib/View/Home/admin-home.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:pointsf/widgets/export-widgets.dart';
 
 class AdminHome extends StatefulWidget {
@@ -18,6 +15,7 @@ class _AdminHomeState extends State<AdminHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: CustomDrawerAdmin(),
       appBar: const CustomAppBarHome(
         title: "Administrativo",
       ),

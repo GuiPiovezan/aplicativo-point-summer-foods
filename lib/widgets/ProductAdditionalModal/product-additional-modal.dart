279f44689ed16e2c5cb9de399edebf50634e1a44
Vitor-Kemuel <kemuvitor@gmail.com>
2022-05-20 23:18:52 -0300

    Implementado seleção de quantidade de itens

--- lib/widgets/ProductAdditionalModal/product-additional-modal.dart
+++ lib/widgets/ProductAdditionalModal/product-additional-modal.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pointsf/Services/CartService/cart-service.dart';
 
+import 'package:pointsf/Services/CartService/cart-service.dart';
 import 'package:pointsf/Services/ProductService/product-service.dart';
 import 'package:pointsf/widgets/export-widgets.dart';
 
@@ -19,6 +19,7 @@ class ProductAdditionalModal extends StatefulWidget {
 
 class _ProductAdditionalModalState extends State<ProductAdditionalModal> {
   final ProductService productSertvice = ProductService();
+  int _itemAmount = 1;
   @override
   void initState() {
     // TODO: implement initState
@@ -31,6 +32,18 @@ class _ProductAdditionalModalState extends State<ProductAdditionalModal> {
         .getAdditionalByCategory(widget.productPrimary["categoria"]);
     setState(() {
       widget.itens = productSertvice.itens;
+    });
+  }
+
+  void _incrementItemAmount() {
+    setState(() {
+      _itemAmount++;
+    });
+  }
+
+  void _decrementItemAmount() {
+    setState(() {
+      _itemAmount > 1 ? _itemAmount-- : null;
     });
   }
 
@@ -54,6 +67,7 @@ class _ProductAdditionalModalState extends State<ProductAdditionalModal> {
     return Container(
       color: Color.fromARGB(255, 74, 44, 82),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
             padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
@@ -81,7 +95,8 @@ class _ProductAdditionalModalState extends State<ProductAdditionalModal> {
                 itemCount: widget.itens.length,
                 itemBuilder: (ctx, index) {
                   return CheckboxListTile(
-                    title: Text(widget.itens[index]["nome"]),
+                    title: Text(
+                        "${widget.itens[index]["nome"]} R\$: ${widget.itens[index]["preco"]}"),
                     subtitle: Text(
                       "Este item ${widget.itens[index]['check'] ? '' : 'não '}está selecionado",
                     ),
@@ -102,17 +117,79 @@ class _ProductAdditionalModalState extends State<ProductAdditionalModal> {
           Container(
             color: Color.fromARGB(255, 240, 240, 240),
             margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-            height: 150,
-            child: CustomTextButton(
-              buttonText: "Adicionar",
-              onPressed: () {
-                CartService().setCartItem(
-                  widget.productPrimary["nome"],
-                  widget.itens,
-                  1,
-                  widget.productPrimary["preco"],
-                );
-              },
+            padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 200, 200, 200),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          bottomLeft: Radius.circular(15),
+                        ),
+                      ),
+                      padding: EdgeInsets.fromLTRB(7, 7, 0, 7),
+                      child: CustomTextButton(
+                        buttonText: "-",
+                        onPressed: _decrementItemAmount,
+                        heigth: 52,
+                        width: 52,
+                      ),
+                    ),
+                    Container(
+                      color: Color.fromARGB(255, 200, 200, 200),
+                      padding: EdgeInsets.fromLTRB(0, 7, 0, 7),
+                      height: 66,
+                      width: 52,
+                      child: Center(
+                        child: Text(
+                          _itemAmount.toString(),
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 200, 200, 200),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                        ),
+                      ),
+                      padding: EdgeInsets.fromLTRB(0, 7, 7, 7),
+                      child: CustomTextButton(
+                        buttonText: "+",
+                        onPressed: _incrementItemAmount,
+                        heigth: 52,
+                        width: 52,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: CustomTextButton(
+                    buttonText: "Adicionar",
+                    onPressed: () {
+                      CartService().setCartItem(
+                        widget.productPrimary["nome"],
+                        widget.itens,
+                        _itemAmount,
+                        widget.productPrimary["preco"],
+                      );
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                )
+              ],
             ),
           )
         ],

3758435fbb83cbcdc7788c9bb4a6ae93dea4e9c7
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-05-24 22:41:25 -0300

    Criando a tela de confirmação de dados

---
+++ lib/View/Orders/confirmation_information.dart
@@ -0,0 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:pointsf/Services/AddressService/address_service.dart';
+import 'package:pointsf/models/address_model.dart';
+
+import '../../widgets/AppBar/custom_appbar.dart';
+
+class ConfirmationAddress extends StatefulWidget {
+  const ConfirmationAddress({Key? key}) : super(key: key);
+
+  @override
+  State<ConfirmationAddress> createState() => _ConfirmationAddressState();
+}
+
+class _ConfirmationAddressState extends State<ConfirmationAddress> {
+  AddressModel? addressSelected;
+  List<AddressModel> addressList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 240, 240, 240),
+      appBar: const CustomAppBar(),
+      body: Column(
+        children: [
+          StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            stream: AddressService().getAddress(),
+            builder: (_, snapshot) {
+              if (!snapshot.hasData) return const CircularProgressIndicator();
+
+              if (snapshot.hasError) {
+                return const Text("Erro ao carregar os dados");
+              }
+
+              if (addressList.isEmpty) {
+                for (var element in snapshot.data!.docs) {
+                  addressList.add(AddressModel.fromMap(element.data()));
+                }
+              }
+
+              return Center(
+                child: DropdownButton(
+                  itemHeight: 70,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 74, 44, 82),
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                  value: addressSelected,
+                  hint: const Text(
+                    "Selecione o endereço de entrega",
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                  elevation: 16,
+                  onChanged: (AddressModel? value) {},
+                  items: addressList.map((AddressModel item) {
+                    return DropdownMenuItem(
+                      value: item,
+                      child: Text(
+                        '${item.logradouro!}, ${item.numero!}',
+                        style: const TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

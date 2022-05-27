7b30c6ed0ab4a859f0b4cbd199b8fe97c46d9aff
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-05-27 16:14:31 -0300

    Criando um Widget personalizado para jogar no conteúdo do container

--- lib/View/Orders/money_payment.dart
+++ lib/View/Orders/money_payment.dart
@@ -1,43 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:pointsf/widgets/export_widgets.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../../widgets/TextFields/custom_text_field.dart';
 
-class MoneyPayment extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
-    double money = 0;
-    return SingleChildScrollView(
-      child: GestureDetector(
-        child: Padding(
-          padding:
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+// ignore: non_constant_identifier_names
+Widget MoneyPayment(BuildContext context) => Wrap(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
           child: Form(
-            key: formKey,
             child: Column(
               children: <Widget>[
                 CustomTextField(
-                  onSaved: (value) => money = double.parse(value!.trim()),
                   labelText: 'Troco',
-                  //validator: (value) => UserValidator.validarNome(value!),
-                ),
-                const SizedBox(height: 8),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
-                  child: CustomTextButton(
-                    buttonText: 'Pronto',
-                    onPressed: () {},
-                  ),
                 ),
               ],
             ),
           ),
         ),
-      ),
+      ],
     );
-  }
-}

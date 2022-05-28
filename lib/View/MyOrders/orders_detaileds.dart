75fda1bdc6d2cd1cdfba19af18a5e50b0c427119
AlexSan1910 <alex.gsan11@gmail.com>
2022-05-28 20:10:33 -0300

    Criação do modal para o card do pedido detalhado

--- lib/View/MyOrders/orders_detaileds.dart
+++ lib/View/MyOrders/orders_detaileds.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:pointsf/models/address_model.dart';
 
 class OrdersDetaileds extends StatelessWidget {
@@ -16,18 +17,97 @@ class OrdersDetaileds extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Container(
-            margin: EdgeInsets.fromLTRB(15, 30, 15, 30),
-            height: MediaQuery.of(context).size.width,
-            width: 380,
-            //color: Color.fromARGB(252, 241, 241, 63),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(
-                color: Color.fromARGB(252, 241, 241, 63),
-                width: 2,
+              margin: EdgeInsets.fromLTRB(15, 15, 15, 15),
+              height: MediaQuery.of(context).size.width,
+              width: MediaQuery.of(context).size.height,
+              //color: Color.fromARGB(252, 241, 241, 63),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(
+                  color: Color.fromARGB(252, 241, 241, 63),
+                  width: 2,
+                ),
               ),
-            ),
-          )
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Produto id - 6016511321616"),
+                        Text("30/Abr")
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Card(
+                        borderOnForeground: true,
+                        color: Colors.blue,
+                      )
+                    ],
+                  ),
+                  Divider(
+                    color: Color.fromARGB(255, 16, 18, 19),
+                    endIndent: 20,
+                    thickness: 1,
+                    height: 10,
+                    indent: 20,
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(Icons.filter_1_sharp),
+                          Text("Açai 300ml c/ 2 Adicionais"),
+                        ]),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [Text("Nutella")],
+                            ),
+                            Row(
+                              children: [Text("Banana")],
+                            )
+                          ],
+                        ),
+                        Text("R 15,00")
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromARGB(255, 16, 18, 19),
+                    endIndent: 20,
+                    thickness: 1,
+                    height: 10,
+                    indent: 20,
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Pagamento no Cartão"),
+                        Row(
+                          children: [
+                            Icon(Icons.credit_card_rounded),
+                            Text("Crédito")
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );

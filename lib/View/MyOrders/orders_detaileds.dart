398ae8788b429d73525d5ab94d5b9fbe1b7a514b
AlexSan1910 <alex.gsan11@gmail.com>
2022-05-28 21:17:54 -0300

    Card do pedido detalhado feito

--- lib/View/MyOrders/orders_detaileds.dart
+++ lib/View/MyOrders/orders_detaileds.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:pointsf/main.dart';
 import 'package:pointsf/models/address_model.dart';
 
 class OrdersDetaileds extends StatelessWidget {
@@ -18,13 +19,13 @@ class OrdersDetaileds extends StatelessWidget {
         children: [
           Container(
               margin: EdgeInsets.fromLTRB(15, 15, 15, 15),
-              height: MediaQuery.of(context).size.width,
-              width: MediaQuery.of(context).size.height,
+              //height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
               //color: Color.fromARGB(252, 241, 241, 63),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 border: Border.all(
-                  color: Color.fromARGB(252, 241, 241, 63),
+                  color: Color.fromARGB(255, 0, 0, 0),
                   width: 2,
                 ),
               ),
@@ -40,14 +41,17 @@ class OrdersDetaileds extends StatelessWidget {
                       ],
                     ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Card(
-                        borderOnForeground: true,
-                        color: Colors.blue,
-                      )
-                    ],
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 5),
+                    child: Card(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.check_circle),
+                          Text("Pedido Concluido as 10:15")
+                        ],
+                      ),
+                    ),
                   ),
                   Divider(
                     color: Color.fromARGB(255, 16, 18, 19),
@@ -57,7 +61,7 @@ class OrdersDetaileds extends StatelessWidget {
                     indent: 20,
                   ),
                   Container(
-                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    margin: EdgeInsets.fromLTRB(60, 10, 60, 0),
                     child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
@@ -66,21 +70,24 @@ class OrdersDetaileds extends StatelessWidget {
                         ]),
                   ),
                   Container(
-                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Column(
                           children: [
-                            Row(
-                              children: [Text("Nutella")],
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                              child: Row(
+                                children: [Text("Nutella")],
+                              ),
                             ),
                             Row(
                               children: [Text("Banana")],
                             )
                           ],
                         ),
-                        Text("R 15,00")
+                        Text("RS 15,00")
                       ],
                     ),
                   ),
@@ -93,6 +100,50 @@ class OrdersDetaileds extends StatelessWidget {
                   ),
                   Container(
                     margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Row(
+                      children: [Text("Soma dos Valores")],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text("SubTotal"), Text("RS 15,00")],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text("Cupom"), Text("RS 4,00")],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text("Taxa de Entrega"), Text("RS 1,50")],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Total"),
+                        Text("RS 12,50"),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromARGB(255, 16, 18, 19),
+                    endIndent: 20,
+                    thickness: 1,
+                    height: 10,
+                    indent: 20,
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -105,7 +156,33 @@ class OrdersDetaileds extends StatelessWidget {
                         )
                       ],
                     ),
-                  )
+                  ),
+                  Divider(
+                    color: Color.fromARGB(255, 16, 18, 19),
+                    endIndent: 20,
+                    thickness: 1,
+                    height: 10,
+                    indent: 20,
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: Row(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.house_rounded),
+                            Text("Endereço")
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                    child: Row(
+                      children: [Text("Av.Tarraf 3576, Portal Cidade Amiga")],
+                    ),
+                  ),
                 ],
               ))
         ],

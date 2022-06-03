12e7c29e8713b9bd5ef531f5a31821e8e1669c74
Vitor-Kemuel <kemuvitor@gmail.com>
2022-06-03 18:36:02 -0300

    Chamando novo widget

--- lib/View/Home/admin_home.dart
+++ lib/View/Home/admin_home.dart
@@ -10,7 +10,8 @@ class AdminHome extends StatefulWidget {
 }
 
 class _AdminHomeState extends State<AdminHome> {
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final PageController _pageController = PageController();
+  int indexNavigatorBar = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +20,68 @@ class _AdminHomeState extends State<AdminHome> {
       appBar: const CustomAppBarHome(
         title: "Administrativo",
       ),
-      body: Container()
+      body: PageView(
+        onPageChanged: (int page) {
+          setState(() {
+            indexNavigatorBar = page;
+          });
+        },
+        controller: _pageController,
+        children: const <Widget>[
+          OrderPage(
+            status: "enviado",
+          ),
+          OrderPage(
+            status: "aceito",
+          ),
+          OrderPage(
+            status: "entregue",
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.fromLTRB(15, 20, 15, 10),
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 74, 44, 82),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.elliptical(500, 50),
+            topRight: Radius.elliptical(500, 50),
+          ),
+        ),
+        child: BottomNavigationBar(
+          onTap: (int index) {
+            setState(() {
+              indexNavigatorBar = index;
+            });
+            _pageController.jumpToPage(
+              index,
+            );
+          },
+          currentIndex: indexNavigatorBar,
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          fixedColor: Colors.green,
+          unselectedItemColor: Colors.white,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.new_label_outlined),
+              label: "Novos",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.verified_outlined),
+              label: "Aceitos",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.airplane_ticket_outlined),
+              label: "Entregues",
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 240, 240, 240),
     );
   }
 }

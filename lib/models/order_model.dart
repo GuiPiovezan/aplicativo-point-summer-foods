fc4f179f0dedf1ea85937ffcf79dfb141234bccb
Vitor-Kemuel <kemuvitor@gmail.com>
2022-06-03 04:15:30 -0300

    alterando model do pedido

--- lib/models/order_model.dart
+++ lib/models/order_model.dart
@@ -9,25 +9,48 @@ class OrderModel {
   AddressModel? address;
   double? valueTotal;
   double? valueDelivery;
-  DateTime? dateOrder;
   TypePayment? typePayment;
   String? moneyChange;
   String? cardFlag;
   String? cardPayment;
   Map<dynamic, dynamic>? itens;
+  DateTime? dateOrder;
+  DateTime? pedidoConcluido;
+  DateTime? pedidoAceito;
 
-  OrderModel(
-      {this.id,
-      this.userName,
-      this.userEmail,
-      this.userPhone,
-      this.address,
-      this.valueTotal,
-      this.valueDelivery,
-      this.dateOrder,
-      this.typePayment,
-      this.cardFlag,
-      this.cardPayment,
-      this.moneyChange,
-      this.itens});
+  OrderModel({
+    this.id,
+    this.userName,
+    this.userEmail,
+    this.userPhone,
+    this.address,
+    this.valueTotal,
+    this.valueDelivery,
+    this.dateOrder,
+    this.typePayment,
+    this.cardFlag,
+    this.cardPayment,
+    this.moneyChange,
+    this.itens,
+    this.pedidoAceito,
+    this.pedidoConcluido,
+  });
+
+  OrderModel.fromMap(Map<String, dynamic> map) {
+    id = map["id"];
+    userName = map["userName"];
+    userEmail = map["userEmail"];
+    userPhone = map["userPhone"];
+    address = map["address"];
+    valueTotal = map["valueTotal"];
+    valueDelivery = map["valueDelivery"];
+    dateOrder = map["dateOrder"];
+    typePayment = map["typePayment"];
+    cardFlag = map["cardFlag"];
+    cardPayment = map["cardPayment"];
+    moneyChange = map["moneyChange"];
+    itens = map["itens"];
+    pedidoAceito = map["pedidoAceito"];
+    pedidoConcluido = map["pedidoConcluido"];
+  }
 }

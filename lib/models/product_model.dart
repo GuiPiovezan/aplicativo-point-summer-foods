016e6ed08fc17dee442269caebd59098b748c70a
Vitor-Kemuel <kemuvitor@gmail.com>
2022-06-03 23:05:36 -0300

    Organizando variaveis dos models em ordem alfabética

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,26 +1,26 @@
 class ProductModel {
   String? categoria;
   String? nome;
+  List<Map<String, dynamic>>? sizes;
   String? status;
   String? tipo;
   String? uid;
-  List<Map<String, dynamic>>? sizes;
 
   ProductModel({
     required this.categoria,
     required this.nome,
+    required this.sizes,
     required this.status,
     required this.tipo,
     required this.uid,
-    required this.sizes,
   });
 
   ProductModel.fromMap(Map<String, dynamic> map) {
     categoria = map['categoria'];
     nome = map['nome'];
+    sizes = map['sizes'];
     status = map['status'];
     tipo = map['tipo'];
     uid = map['uid'];
-    sizes = map['sizes'];
   }
 }
